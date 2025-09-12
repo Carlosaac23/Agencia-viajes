4c64bcf250355355725756fbc80858fde06fc16b
Carlos <carlosaac232001@gmail.com>
2025-09-11 21:24:09 -0500

    feat: add viajes and testimoniales routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,27 @@ const router = express.Router();
 
 // req - Lo que enviamos. res - Lo que express nos responde
 router.get('/', (req, res) => {
-  res.render('inicio');
+  res.render('inicio', {
+    pagina: 'Inicio',
+  });
 });
 
 router.get('/nosotros', (req, res) => {
-  res.render('nosotros');
+  res.render('nosotros', {
+    pagina: 'Nosotros',
+  });
+});
+
+router.get('/viajes', (req, res) => {
+  res.render('viajes', {
+    pagina: 'Viajes',
+  });
+});
+
+router.get('/testimoniales', (req, res) => {
+  res.render('testimoniales', {
+    pagina: 'Testimoniales',
+  });
 });
 
 export default router;
