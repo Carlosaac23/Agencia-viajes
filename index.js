6197802e4db632304ca7b5562a417d3f2acfb762
Carlos <carlosaac232001@gmail.com>
2025-09-11 20:50:51 -0500

    feat: set main assets and component into root index

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 // Definir puerto
 const port = process.env.PORT || 4000;
 
-app.listen(() => {
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Definiar la carpeta pública
+app.use(express.static('public'));
+
+// Agregar Router
+app.use('/', router);
+
+app.listen(port, () => {
   console.log(`El servidor está funcionando en el puerto ${port}`);
 });
