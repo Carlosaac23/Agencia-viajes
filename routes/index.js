6ecb995ff8048cf370655e60cc9c0fc7cdfa7523
Carlos <carlosaac232001@gmail.com>
2025-09-15 15:22:26 -0500

    feat: add form POST method route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import {
   paginaTestimoniales,
   paginaDetalleViaje,
 } from '../controllers/paginasController.js';
+import { guardarTestimonial } from '../controllers/testimonialController.js';
 
 const router = express.Router();
 
@@ -19,5 +20,6 @@ router.get('/viajes', paginaViajes);
 router.get('/viajes/:slug', paginaDetalleViaje);
 
 router.get('/testimoniales', paginaTestimoniales);
+router.post('/testimoniales', guardarTestimonial);
 
 export default router;
